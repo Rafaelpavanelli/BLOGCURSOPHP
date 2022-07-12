470de0f0de9a125fbfb17fa4f5ed4506deb402bb
Rafael <rafaelzica86@hotmail.com>
2022-07-12 13:11:43 -0300

    Necessito de ajuda para entender porque esta dando erro na hora dos include_onde nos outros arquivos

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+include_once("templates/header.php");
+
+?>
+
+
+  <h1>ME mata senhor </h1>
+
+  <?php 
+include_once("templates/contato.php");
+?>
---
+++ templates/contato.php
@@ -0,0 +1,8 @@
+<?php
+include_once("header.php");
+
+
+?>
+<footer>
+  <p>Hora de codar</p>
+</footer>
